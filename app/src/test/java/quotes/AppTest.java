f50d1c3cb8419339b23d5a12cfef13d4e9e8ee87
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-08-04 21:38:03 +0300

    lab 08 done

--- app/src/test/java/quotes/AppTest.java
+++ app/src/test/java/quotes/AppTest.java
@@ -4,11 +4,21 @@
 package quotes;
 
 import org.junit.jupiter.api.Test;
+import static quotes.App.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+    @Test void testqoutes(){
+        Path path1= Paths.get("src/main/resources/test.json");
+        assertEquals("Quotes{tags=[attributed-no-source], author='Marilyn Monroe', likes='18651 likes', text=' “I am good, but not an angel. I do sin, but I am not the devil. I am just a small girl in a big world trying to find someone to love.” '}",generateRandomQuote(path1));
+
     }
 }
